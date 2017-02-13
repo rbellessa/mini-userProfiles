c2a1d9f3ed6aa4d8fdeb1d33693d3d761167c673
Ryan Bellessa <rbellessa@gmail.com>
2017-02-13 06:11:52 -0700

    initial commit

--- controller.js
+++ controller.js
@@ -1,5 +1,9 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
-})
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
+
+  $scope.thisAppIsBroken = "This angular app is working and below are my friends";
+
+  $scope.users = mainService.getUsers();
+
+  $scope.toggleFavorite = mainService.toggleFavorite;
+
+});
